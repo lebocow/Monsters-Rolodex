b9c9203827ef55f1b4f06152febffb7f37854291
Ursachianu Ionut-Alexandru <tzmic1@gmail.com>
2023-01-07 15:26:39 +0200

    Adding css to search-box

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
     return (
       <div className="App">
         <SearchBox
-          className="SearchBox"
+          className="monsters-search-box"
           onChangeHandler={onSearchChange}
           placeholder="SearchMonsters"
         />
